02932acb531f993834e563fd7c7ad3921160ab7a
Sagarkumarsharma <54928147+Sagarkumarsharma@users.noreply.github.com>
2023-12-12 01:50:43 +0530

    Delete first_djangoProject directory

--- first_djangoProject/django_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-#register your models here
-from .models import Post
-admin.site.register(Post) #to register the post in the administration
--- first_djangoProject/users_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    name = 'users_app'
-
-    def ready(self):
-        import users_app.signals  #import signals.py
